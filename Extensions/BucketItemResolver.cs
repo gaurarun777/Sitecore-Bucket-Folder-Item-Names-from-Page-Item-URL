0f649ffbb05d7695d8fd408f147fda0af967785e
Arun Sharma <arun.sharma1@bhp.com>
2021-12-15 18:42:45 +0530

    intial commit

---
+++ Extensions/BucketItemResolver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Sitecore.Pipelines.HttpRequest;
+using Sitecore;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.SearchTypes;
+using System.Linq;
+
+namespace Sitecore.Foundation.SitecoreExtensions.Extensions
+{
+    public class BucketItemResolver : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            if (Context.Item != null || Context.Database == null || args.Url.ItemPath.Length == 0)
+                return;
+            var requestUrl = args.Url.ItemPath.TrimEnd('/');
+
+            var index = requestUrl.LastIndexOf('/');
+
+            var bucketPath = requestUrl.Substring(0, index);
+            //bucketPath = MainUtil.DecodeName(bucketPath);
+            var bucketItem = args.GetItem(bucketPath);
+            if (bucketItem != null)
+            {
+                var itemName = requestUrl.Substring(index + 1);
+                var searchIndex = ContentSearchManager.GetIndex(Sitecore.Configuration.Settings.GetSetting("Sitecore.Foundation.SitecoreExtensions.ArticleIndexName"));
+                using (var searchContext = searchIndex.CreateSearchContext())
+                {
+                    Sitecore.Data.ID bucketTemplateID = new Sitecore.Data.ID(new Guid(Sitecore.Configuration.Settings.GetSetting("Sitecore.Foundation.SitecoreExtensions.ArticleTemplateId")));
+                    var result = searchContext.GetQueryable<SearchResultItem>().Where(
+                                x => x.TemplateId == bucketTemplateID && (x.Name == itemName || x.Name == itemName.Replace("-", " "))).FirstOrDefault();
+
+                    if (result != null)
+                    {
+                        var item = result.GetItem();
+                        if (item.Language == Context.Language)
+                        {
+                            Context.Item = result.GetItem();
+                        }
+                        else
+                        {
+                            var langItem = Sitecore.Context.Database.GetItem(result.ItemId, Context.Language);
+                            if (langItem != null)
+                            {
+                                Context.Item = langItem;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Extensions/BucketLinkManager.cs
@@ -0,0 +1,59 @@
+﻿using Sitecore.Buckets.Managers;
+using Sitecore.Buckets.Extensions;
+using Sitecore.IO;
+using Sitecore.Links;
+using Sitecore.Links.UrlBuilders;
+
+namespace Sitecore.Foundation.SitecoreExtensions.Extensions
+{
+    public class BucketLinkManager : ItemUrlBuilder
+    {
+        public BucketLinkManager(DefaultItemUrlBuilderOptions defaultOptions) : base(defaultOptions)
+        {
+        }
+
+        public override string Build(Sitecore.Data.Items.Item item, ItemUrlBuilderOptions options)
+        {
+            if (BucketManager.IsItemContainedWithinBucket(item))
+            {
+                var bucketItem = item.GetParentBucketItemOrParent();
+                if (bucketItem != null && bucketItem.IsABucket())
+                {
+                    //var bucketUrl = base.Build(bucketItem, options);
+                    //for getting parent of article folder
+                    var bucketUrl = base.Build(bucketItem.Parent, options);
+                    if ((bool)options.AddAspxExtension)
+                    {
+                        bucketUrl = bucketUrl.Replace(".aspx", string.Empty);
+                    }
+
+                    return FileUtil.MakePath(bucketUrl, item.Name).Replace(" ", "-") +
+                            ((bool)options.AddAspxExtension ? ".aspx" : string.Empty);
+                }
+            }
+
+            return Sitecore.StringUtil.EnsurePostfix('/', base.Build(item, options));
+        }
+
+        //public override string GetItemUrl(Sitecore.Data.Items.Item item, ItemUrlBuilderOptions options)
+        //{
+        //    if (BucketManager.IsItemContainedWithinBucket(item))
+        //    {
+        //        var bucketItem = item.GetParentBucketItemOrParent();
+        //        if (bucketItem != null && bucketItem.IsABucket())
+        //        {
+        //            var bucketUrl = base.GetItemUrl(bucketItem, options);
+        //            if (options.AddAspxExtension)
+        //            {
+        //                bucketUrl = bucketUrl.Replace(".aspx", string.Empty);
+        //            }
+
+        //            return FileUtil.MakePath(bucketUrl, item.Name).Replace(" ", "-") +
+        //                    (options.AddAspxExtension ? ".aspx" : string.Empty);
+        //        }
+        //    }
+
+        //    return Sitecore.StringUtil.EnsurePostfix('/', base.GetItemUrl(item, options));
+        //}
+    }
+}
